3514c52d55075026a0f79b91fb13f2844347e32a
hinatakato0205 <hinata20082012@gmail.com>
2026-09-14 15:34:11 +0900

    cargo関連のファイルを作った

---
+++ rust_projects/hello_cargo/target/debug/deps/hello_cargo-89bf991e1ad42f10.d
@@ -0,0 +1,5 @@
+/home/siwuser/repositories/2026-second-26010013-hinata/rust_projects/hello_cargo/target/debug/deps/hello_cargo-89bf991e1ad42f10.d: src/main.rs
+
+/home/siwuser/repositories/2026-second-26010013-hinata/rust_projects/hello_cargo/target/debug/deps/hello_cargo-89bf991e1ad42f10: src/main.rs
+
+src/main.rs:
---
+++ rust_projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/siwuser/repositories/2026-second-26010013-hinata/rust_projects/hello_cargo/target/debug/hello_cargo: /home/siwuser/repositories/2026-second-26010013-hinata/rust_projects/hello_cargo/src/main.rs
